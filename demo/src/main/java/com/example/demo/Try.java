7731612c45be2e6baeef97371c38128303e90f4c
pranitrathod <pranitrathodsocial@gmail.com>
2024-02-04 16:10:54 +0530

    Restarted the workflow!

--- demo/src/main/java/com/example/demo/Try.java
+++ demo/src/main/java/com/example/demo/Try.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloSpring {
-@GetMapping(path = "")
-public String helloSpringBoot()
-{
-return "<h1>Hello Spring Boot!</h1>";}
+public class Try {
+    @GetMapping(path = "")
+    String say()
+    {
+        return "FUCK YOU!";
+    }
 }
-
